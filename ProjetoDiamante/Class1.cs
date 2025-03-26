2f541b355c54400a574e884398a1dd505f646e3c
leandroJader <ldandro>
2025-03-26 09:26:40 -0300

    versão 03, implementada refatoração de codigo, adicionada nova classe para funcionamento de das funções separadamente da program, implementada opção de escolha do úsuario para continuar a utilização do programa

---
+++ ProjetoDiamante/Class1.cs
@@ -0,0 +1,68 @@
+﻿namespace ProjetoDiamante;
+
+    public class FuncoesDiamante
+    {
+      
+        public static int SolicitarEntradaDeDados()
+        {
+            int tamanhoDiamante;
+                Console.WriteLine("Informe um número ímpar para o desenho do diamante:");
+     
+        while (true)
+            {
+              
+
+           
+                if (!int.TryParse(Console.ReadLine(), out tamanhoDiamante))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Entrada inválida. Digite um número inteiro.");
+                    Console.ResetColor();
+                    continue;
+                }
+                Console.WriteLine();
+            
+                if (tamanhoDiamante % 2 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Número inválido. Digite um número ímpar.");
+                    Console.ResetColor();
+                    continue;
+                }
+               Console.WriteLine();
+                return tamanhoDiamante;
+            }
+        }
+
+        public static void DesenharDiamante(int tamanho)
+        {
+            int meio = tamanho / 2;
+
+            for (int i = 0; i <= meio; i++)
+            {
+                int espacos = meio - i;
+                int quantidadeDeX = 2 * i + 1;
+               
+                Console.WriteLine(new string(' ', espacos) + new string('x', quantidadeDeX));
+            }
+
+            for (int i = meio - 1; i >= 0; i--)
+            {
+                int espacos = meio - i;
+                int quantidadeDeX = 2 * i + 1;
+            
+                Console.WriteLine(new string(' ', espacos) + new string('x', quantidadeDeX));
+            }
+            
+            
+        }
+    public static bool Continuar()
+    {
+       
+        Console.WriteLine("você deseja continuar ? S/N");
+       string  continuar = Console.ReadLine().ToUpper();
+
+            return continuar == "S";
+        
+    }
+    }
--- ProjetoDiamante/Program.cs
+++ ProjetoDiamante/Program.cs
@@ -1,51 +1,22 @@
-﻿using System;
+﻿using ProjetoDiamante;
 
-namespace ProjetoDiamante
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
-            Console.WriteLine("Informe um número ímpar para verificar o desenho do diamante:");
+   
+            int tamanhoDiamante = FuncoesDiamante.SolicitarEntradaDeDados();
+        
+            FuncoesDiamante.DesenharDiamante(tamanhoDiamante);
 
-            while (true)
+            if (!FuncoesDiamante.Continuar())
             {
-                int tamanhoDiamante;
-
-                if (!int.TryParse(Console.ReadLine(), out tamanhoDiamante))
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Entrada inválida. Digite um número inteiro.");
-                    Console.ResetColor();
-                    continue;
-                }
-
-                if (tamanhoDiamante % 2 == 0)
-                {
-                    Console.WriteLine("Número inválido, digite um número ímpar.");
-                    continue;
-                }
-
-                int meio = tamanhoDiamante / 2;
-
-                
-                for (int i = 0; i <= meio; i++)
-                {
-                    int espacos = meio - i;
-                    int quantidadeDeX = 2 * i + 1;
-                    Console.WriteLine(new string(' ', espacos) + new string('x', quantidadeDeX));
-                }
-
-      
-                for (int i = meio - 1; i >= 0; i--)
-                {
-                    int espacos = meio - i;
-                    int quantidadeDex = 2 * i + 1;
-                    Console.WriteLine(new string(' ', espacos) + new string('x', quantidadeDex));
-                }
-
-                Console.ReadLine();
+                break;
             }
+
+            
         }
     }
 }
